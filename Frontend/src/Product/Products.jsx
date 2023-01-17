8e2af87939848abe43a9bec807f95d0349278a6b
ajayahara <abak00350@gmail.com>
2023-01-17 14:20:07 +0530

    Product left side template done

--- Frontend/src/Product/Products.jsx
+++ Frontend/src/Product/Products.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import "./ProductCss/Products.css"
+import { ProductsLeft } from './ProductsLeft'
+import ProductsRight from './ProductsRight'
 const Products = () => {
   return (
-    <div>Products</div>
+    <div className='a-products'>
+          <ProductsLeft/>
+          <ProductsRight/>
+    </div>
   )
 }
 
---
+++ Frontend/src/Product/ProductsRight.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ProductCss/ProductsRight.css"
+
+const ProductsRight = () => {
+  return (
+    <div className='a-products-right'>
+    
+    </div>
+  )
+}
+
+export default ProductsRight
