d5cf60ba114873de858daad8ae67fdd650377e1f
Vivek Anjan <vivekanjankc12345@gmail.com>
2023-01-20 00:50:26 +0530

    done

--- Frontend/src/Home/Footer.js
+++ Frontend/src/Home/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import "./Footer.css"
 const Footer = () => {
   return (
-    <div>
-      Footer
+   <>
+    <div className='footer1'>
+      <div className='footer-a'></div>
+      <div className='footer-b '></div>
+      <div className='footer-c'></div>
+      <div className='footer-d'></div>
     </div>
+    <div className='footer2'>
+<h1>All Rights Reserved Powered by ANS Commerce</h1>
+    </div>
+   </>
   )
 }
 
