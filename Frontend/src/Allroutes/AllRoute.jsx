d601aedfee9238d0c7c7e4d8ddc67668220e3e2c
ajayahara <abak00350@gmail.com>
2023-01-21 01:16:15 +0530

    login payment register page added

--- Frontend/src/Allroutes/AllRoute.jsx
+++ Frontend/src/Allroutes/AllRoute.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import {Route,Routes} from "react-router-dom"
 import MainHome from '../Home/MainHome'
+import { Login } from '../Login/Login'
+import { Payment } from '../Payment/Payment'
 import Products from '../Product/Products'
+import { Register } from '../Register/Register'
 import { SingleProduct } from '../SingleProduct/SingleProduct'
 export const AllRoute = () => {
   return (
     <Routes>
         <Route path='/' element={<MainHome/>}></Route>
-        <Route path='/register' element={<div>Register</div>}></Route>
-        <Route path='/login' element={<div>Login</div>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
         <Route path='/products' element={<Products/>}></Route>
         <Route path='/products/:id' element={<SingleProduct/>}></Route>
         <Route path='/cart' element={<div>Cart</div>}></Route>
-        <Route path='/payment' element={<div>Payment</div>}></Route>
-        <Route path='/profile' element={<div>Profile</div>}></Route>
+        <Route path='/payment' element={<Payment/>}></Route>
         <Route path='*' element={<div>No Page Found</div>}></Route>
     </Routes>
   )
