51a287f0ec928ef46e98af0d872570715938514a
Suraj Pratap <advocatesurajpratap95@gmail.com>
2023-01-22 09:57:48 +0530

    cart

--- Frontend/src/Allroutes/AllRoute.jsx
+++ Frontend/src/Allroutes/AllRoute.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {Route,Routes} from "react-router-dom"
 import Products from '../Product/Products'
 import { SingleProduct } from '../SingleProduct/SingleProduct'
+
+import Cart from "../Cart/Cart"
+
 export const AllRoute = () => {
   return (
     <Routes>
@@ -10,7 +13,7 @@ export const AllRoute = () => {
         <Route path='/login' element={<div>Login</div>}></Route>
         <Route path='/products' element={<Products/>}></Route>
         <Route path='/products/:id' element={<SingleProduct/>}></Route>
-        <Route path='/cart' element={<div>Cart</div>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
         <Route path='/payment' element={<div>Payment</div>}></Route>
         <Route path='/profile' element={<div>Profile</div>}></Route>
         <Route path='*' element={<div>No Page Found</div>}></Route>
