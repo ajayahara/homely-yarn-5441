92fabb1cd2d62a1a7079c6410d7123e58c26a904
Suraj Pratap <advocatesurajpratap95@gmail.com>
2023-01-22 14:45:42 +0530

    sp

--- Frontend/src/SingleProduct/SingleProduct.jsx
+++ Frontend/src/SingleProduct/SingleProduct.jsx
@@ -3,6 +3,10 @@ import { useEffect } from 'react';
 import {Row,Col,ListGroup,Button, Image, ListGroupItem} from "react-bootstrap"
 import { useParams } from 'react-router-dom';
 export const SingleProduct = () => {
+  const handleadd=(_id)=>{
+    localStorage.setItem("id",(_id))
+   console.log(_id)
+  }
   let {id}=useParams()
   // const _id=id
   // console.log(_id)
@@ -21,7 +25,7 @@ export const SingleProduct = () => {
   },[])
 // console.log(params)
   return (
-    <div>
+    <div style={{display:"flex",justifyContent:"center",alignItem:"center",marginTop:"20px"}}>
       <Row>
         <Col  mD={6}>
           <Image src={data ?.image_url}  alt={data?.name} fluid/>
@@ -29,24 +33,28 @@ export const SingleProduct = () => {
         <Col mD={3}>
           <ListGroup variant="flush">
             <ListGroupItem>
-              <h3>{data?.name}</h3>
+              <h3>Title:{data?.name}</h3>
             </ListGroupItem>
             <ListGroupItem>
-              <h3> Price : $ {data?.price}</h3>
+              <h3> Price : $ {data?.actualPrice}</h3>
             </ListGroupItem>
             <ListGroupItem>
               <h3>{data?.description}</h3>
             </ListGroupItem>
             <ListGroupItem>
-              <h3>{data?.color}</h3>
+              <h3>Color:{data?.color}</h3>
             </ListGroupItem>
           </ListGroup>
         </Col>
         <Col>
         <Row>
-          <col>
+          {/* <col>
           <Button>ADD TO CART</Button>
-          </col>
+          </col> */}
+          <ListGroupItem>
+              <Button style={{backgroundColor:"rgb(201,4,143)",color:"white",padding:"10px 10px",borderRadius:"5px"}} 
+              className='btn_block' type="button" onClick={()=>handleadd(data._id)}>Add to Cart</Button>
+            </ListGroupItem>
         </Row>
         </Col>
       </Row>
